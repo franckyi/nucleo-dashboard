d1ae2521fce4ca795e26dbab5499e51954d9715e
Francky <contact@francky.pl>
2024-01-02 18:42:40 +0100

    add clients view

--- src/app/clients/ClientsTable.tsx
+++ src/app/clients/ClientsTable.tsx
@@ -6,55 +6,61 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  location: string,
+  firstInquiry: string,
+  totalInquiries: number,
+  totalPayed: number,
+  email: string,
+  isActive: boolean,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { name, location, firstInquiry, totalInquiries, totalPayed, email, isActive };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Nike', 'Baltimore\, US', '2019/01/23', 2, 3000, 'nike@email.com', true),
+  createData('Jeep', 'New York\, US', '2020/08/02', 4, 13500, 'jeep@email.com', true),
+  createData('Lebron James', 'Los Angeles\, US', '2022/03/31', 1, 3500, 'lj@email.com', true),
+  createData('Coca Cola', 'Paris\, FR', '2016/12/20', 1, 7000, 'cocacola@email.com', true),
+  createData('Some Startup somewhere', 'Torino\, IT', '2023/07/25', 7, 9000, 'somestartup@email.com', true),
 ];
 
 export default function ClientsTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <>
+      <TextField id="filled-basic" label="Search by name" variant="filled" />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Company</TableCell>
+              <TableCell align="right">Location</TableCell>
+              <TableCell align="right">First inquiry date</TableCell>
+              <TableCell align="right">Total inquiries</TableCell>
+              <TableCell align="right">Total payed</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.location}</TableCell>
+                <TableCell align="right">{row.firstInquiry}</TableCell>
+                <TableCell align="right">{row.totalInquiries}</TableCell>
+                <TableCell align="right">{row.totalPayed}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
