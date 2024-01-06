97c7cce0ce9d44d45b5dda41977c0591f41eb60d
Francky <contact@francky.pl>
2024-01-06 18:58:07 +0100

    add antialiased to font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Play } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Play } from "next/font/google";
+import "./globals.css";
 
-const play = Play({ subsets: ['latin-ext'], weight: ["400", "700"] })
+const play = Play({ subsets: ["latin-ext"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: 'Nucleo B2B',
-  description: 'A full featured dashboard for freelancers and startups',
-}
+  title: "Nucleo B2B",
+  description: "A full featured dashboard for freelancers and startups",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={play.className}>{children}</body>
+      <body className={`${play.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
