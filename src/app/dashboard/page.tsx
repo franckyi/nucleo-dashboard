2aa4d7e7d4173d54f42128eb411648f172a29bba
Francky <contact@francky.pl>
2024-01-09 21:32:45 +0100

    remove corrupted component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import MainStats from "../components/MainStats";
-
 export default function Page() {
   return (
     <>
       <h1>Dashboard Page works!</h1>
-      <MainStats />
       <p>Show main stats here</p>
     </>
   );
