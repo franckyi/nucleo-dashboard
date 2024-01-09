3b6743dbd790eff2a27c1cfd2f85ad769ed8aec2
Francky <contact@francky.pl>
2024-01-09 20:41:21 +0100

    change the home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Dashboard from "./components/Dashboard";
-
 export default function Home() {
   return (
-    <Dashboard />
-  )
+    <main className="flex min-h-screen flex-col p-6 bg-stone-200 text-stone-900">
+      <h1>A great tool for freelancers is coming soon..</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        assumenda facilis animi quidem hic iste possimus officia, sapiente fugit
+        alias voluptas voluptatum veniam numquam unde, aut minus, recusandae
+        temporibus rerum.
+      </p>
+    </main>
+  );
 }
