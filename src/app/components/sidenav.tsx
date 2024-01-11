99be821d151bb79eb758f6a43c9e269903dea1ac
Francky <contact@francky.pl>
2024-01-11 22:55:52 +0100

    change colors

--- src/app/components/sidenav.tsx
+++ src/app/components/sidenav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import NavLinks from "@/app/components/nav-links";
 import { PowerIcon } from "@heroicons/react/24/outline";
 
@@ -6,10 +7,18 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-emerald-400 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-violet-300 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">Nucleo logo</div>
+        <Image
+          src="/logo.svg"
+          className="mr-2"
+          width={38}
+          height={34}
+          alt="logo"
+          draggable="false"
+        ></Image>
+        <div className="w-32 text-white md:w-40">Nucleo B2B</div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 text-stone-800">
         <NavLinks />
